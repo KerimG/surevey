31bcacc8396aaea853d1b749ea6fec969f7acb02
Kerim Güney <kerim@gueney.org>
2018-03-24 21:21:58 +0100

    change port to default http port

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,8 @@
-const https = require("https");
 const express = require("express");
-const sqlite = require("sqlite3");
 
 let app = express();
 
 app.use(express.static("static"));
 
-app.listen(8080);
+console.log("Express app listening on port 80");
+app.listen(80);
