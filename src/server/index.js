a4a0f2923ebee4f3789e68e59e680775126be62c
Kerim Güney <kerim@gueney.org>
2018-03-25 10:56:40 +0200

    add comments and console message

--- src/server/index.js
+++ src/server/index.js
@@ -7,11 +7,13 @@ let app = express();
 app.use(express.static(__dirname + "/static"));
 
 if (process.env.NODE_ENV === "production") {
+  // create symlinks to ssl files in server/ssl folder
   let httpsOptions = {
     key: fs.readFileSync(__dirname + "/ssl/privkey.pem"),
     cert: fs.readFileSync(__dirname + "/ssl/cert.pem")
   };
 
+  console.log("Express app listening on SSL enabled port 443");
   https.createServer(httpsOptions, app).listen(443);
 } else {
   console.log("Express app listening on port 8080");
