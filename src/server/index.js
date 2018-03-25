c4b6d9863cc5246074963c26e0b476d53c263c02
Kerim Güney <kerim@gueney.org>
2018-03-25 10:18:54 +0200

    add basic SSL support

--- src/server/index.js
+++ src/server/index.js
@@ -4,5 +4,14 @@ let app = express();
 
 app.use(express.static(__dirname + "/static"));
 
-console.log("Express app listening on port 80");
-app.listen(80);
+if (process.env.NODE_ENV === "production") {
+  let httpsOptions = {
+    key: fs.readFileSync(__dirname + "/ssl/privatekey.pem"),
+    cert: fs.readFileSync(__dirname + "/ssl/certificate.pem")
+  };
+
+  https.createServer(httpsOptions, app).listen(443);
+} else {
+  console.log("Express app listening on port 8080");
+  app.listen(8080);
+}
